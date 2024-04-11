7538024e42dedd911913c0e5f2711be9a62a7a17
vdarsha <66336210+vdarsha@users.noreply.github.com>
2024-04-11 14:24:14 -0700

    wgrep done

--- project1/initial-utilities/wgrep/wgrep.cpp
+++ project1/initial-utilities/wgrep/wgrep.cpp
@@ -10,27 +10,53 @@
 using namespace std;
 int main(int argc, char *argv[]) {
     // Check if there are any arguments
-    if (__argc == 1) {
+    if (argc == 1) {
         cout << "wgrep: searchterm [file ...]" << endl;
         return 1;
     }
-    // Loop through all the arguments
-    for (int i = 2; i < __argc; i++) {
-        int verify = open(argv[i], O_RDONLY);
-        // check if open was successful
-        if(verify < 0){
-            cerr << "wgrep: cannot open file" << endl;
-            return 1;
-        }
-        // read & print the file
-        char buffer[1024];
-        int bytesread = read(verify, buffer, sizeof(buffer));
-        while (bytesread > 0) {
-            write(STDOUT_FILENO, buffer, bytesread);
+    string searchterm = argv[1];
+    // Loop through all the files if there are any
+    if (argc > 2) {
+        for (int i = 2; i < argc; i++) {
+            int verify = open(argv[i], O_RDONLY);
+            // check if open was successful
+            if(verify < 0){
+                cout << "wgrep: cannot open file" << endl;
+                exit(1);
+            }
+            // read & print the file
+            char buffer[1];
+            int bytesread = read(verify, buffer, sizeof(buffer));
+            string bufferst = "";
+            while (bytesread > 0) {
+            bufferst += buffer[0];
+            if(buffer[0] == '\n'){
+                if(bufferst.find(searchterm) != string::npos){
+                    write(STDOUT_FILENO, bufferst.c_str(), bufferst.size());
+                }
+                bufferst = "";
+            }
             bytesread = read(verify, buffer, sizeof(buffer));
-        }   
-        // close the file
-        verify = close(verify);
+        }
+            // close the file
+            verify = close(verify);
+        }
     }
+    else{
+        // read from stdin
+        char buffer[1];
+        int bytesread = read(STDIN_FILENO, buffer, sizeof(buffer));
+        string bufferst = "";
+        while (bytesread > 0) {
+            bufferst += buffer[0];
+            if(buffer[0] == '\n'){
+                if(bufferst.find(searchterm) != string::npos){
+                    write(STDOUT_FILENO, bufferst.c_str(), bufferst.size());
+                }
+                bufferst = "";
+            }
+            bytesread = read(STDIN_FILENO, buffer, sizeof(buffer));
+        }
+    }   
     return 0;
 }
